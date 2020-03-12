e5d087bd7946e23c641906ec890ebf7427d3ae76
betler <17320117+betler@users.noreply.github.com>
2020-03-12 17:25:15 +0100

    #1 Project creation

---
+++ src/main/java/cvitae/pro/gmailrelayer/GMailRelayerApplication.java
@@ -0,0 +1,13 @@
+package cvitae.pro.gmailrelayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GMailRelayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GMailRelayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cvitae/pro/gmailrelayer/GMailRelayerApplicationTests.java
@@ -0,0 +1,13 @@
+package cvitae.pro.gmailrelayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GMailRelayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
