229a1cbd76f40f958e0f941b0aa909a0640f9b68
BeTLeR <17320117+betler@users.noreply.github.com>
2020-04-05 13:04:00 +0200

    #8

--- src/main/java/pro/cvitae/gmailrelayer/GMailRelayerApplication.java
+++ src/main/java/pro/cvitae/gmailrelayer/GMailRelayerApplication.java
@@ -1,4 +1,4 @@
-package cvitae.pro.gmailrelayer;
+package pro.cvitae.gmailrelayer;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
@@ -10,7 +10,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = { "io.swagger", "cvitae.pro.gmailrelayer.api", "cvitae.pro.gmailrelayer" })
+@ComponentScan(basePackages = { "io.swagger", "cvitae.pro.gmailrelayer.api", "cvitae.pro.gmailrelayer",
+        "pro.cvitae.gmailrelayer" })
 public class GMailRelayerApplication implements CommandLineRunner {
 
     @Override
@@ -20,7 +21,7 @@ public class GMailRelayerApplication implements CommandLineRunner {
         }
     }
 
-    public static void main(final String[] args) throws Exception {
+    public static void main(final String[] args) {
         new SpringApplication(GMailRelayerApplication.class).run(args);
     }
 
