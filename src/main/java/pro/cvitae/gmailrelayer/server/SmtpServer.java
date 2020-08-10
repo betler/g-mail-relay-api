fd0b702b38b0df9af6671948e7dd2ee779aca4df
BeTLeR <17320117+betler@users.noreply.github.com>
2020-08-10 23:10:19 +0200

    Removed NoopMetricFactory from Apache James

--- src/main/java/pro/cvitae/gmailrelayer/server/SmtpServer.java
+++ src/main/java/pro/cvitae/gmailrelayer/server/SmtpServer.java
@@ -20,7 +20,6 @@ package pro.cvitae.gmailrelayer.server;
 import java.net.InetSocketAddress;
 import java.util.Collection;
 
-import org.apache.james.metrics.api.NoopMetricFactory;
 import org.apache.james.protocols.api.Protocol;
 import org.apache.james.protocols.api.handler.ProtocolHandler;
 import org.apache.james.protocols.netty.NettyServer;
@@ -58,7 +57,7 @@ public class SmtpServer {
         smtpConfiguration.setSoftwareName("Spring Boot g-mail-relayer SMTP Server");
         smtpConfiguration.setUseAddressBracketsEnforcement(false);
 
-        final SMTPProtocolHandlerChain chain = new SMTPProtocolHandlerChain(new NoopMetricFactory());
+        final SMTPProtocolHandlerChain chain = new SMTPProtocolHandlerChain(null);
         chain.addAll(this.handlers);
         chain.wireExtensibleHandlers();
 
